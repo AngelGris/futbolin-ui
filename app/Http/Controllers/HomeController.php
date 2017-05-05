c64d484e29a5d4e5a83c891e59daf2a4579f9920
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-05-05 19:28:28 -0300

    Added strategy to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,15 +24,30 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $team = Auth::user()->team;
 
-        if (is_null(Auth::user()->team)) {
+        if (is_null($team)) {
             return redirect('/equipo/crear');
+        }
+
+        $players = [];
+        foreach ($team->players as $player) {
+            $players[$player['id']] = $player;
+        }
+
+        $strategy = [];
+        for ($i = 1; $i <= 11; $i++) {
+            $strategy[$i - 1]['left'] = (int)($team->strategy->{sprintf('j%02d_start_y', $i)} * 1.11);
+            $strategy[$i - 1]['top'] = (int)(100 - $team->strategy->{sprintf('j%02d_start_x', $i)} * 1.11);
         }
 
         $vars = [
             'icon' => 'fa fa-home',
             'title' => 'Vestuario',
-            'subtitle' => 'Aquí comienza todo'
+            'subtitle' => 'Aquí comienza todo',
+            'formation' => $team->formation,
+            'players' => $players,
+            'strategy' => $strategy,
         ];
 
         return view('home', $vars);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.inner')
 
 @section('content-inner')
-            Content here
+<div class="col-md-6" id="home-container">
+    <div id="home-strategy">
+        <img src="{{ asset('/img/field-large-h.png') }}" />
+        @for ($i = 0; $i < 11; $i++)
+            @if (empty($formation[$i]))
+            <div class="player-container" style="left:{{ $strategy[$i]['left'] }}%;top:{{ $strategy[$i]['top'] }}%;"></div>
+            @else
+            <div class="player-container {{ strtolower($players[$formation[$i]]['position']) }}" style="left:{{ $strategy[$i]['left'] }}%;top:{{ $strategy[$i]['top'] }}%;">{{ $players[$formation[$i]]['number'] }}</div>
+            @endif
+        @endfor
+        <a href="{{ route('strategy') }}"><div class="overlay"></div></a>
+    </div>
+</div>
 @endsection
