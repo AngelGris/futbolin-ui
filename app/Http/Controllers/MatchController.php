74800156807fb8850f9ee0fa32f7d6be2b414739
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-05-05 11:17:36 -0300

    Added text_color to team and make the Python integration to simmulate the matches

---
+++ app/Http/Controllers/MatchController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Team;
+
+class MatchController extends Controller
+{
+    /**
+     * Play match
+     */
+    public function play(Request $request)
+    {
+        $file_name = $_SERVER['REQUEST_TIME'] . '-' . Auth::user()->team->id . '-' . $request->rival . '.log';
+        $command = escapeshellcmd('python3 ' . base_path() . '/python/play.py ' . Auth::user()->team->id . ' ' . $request->rival . ' 0 ' . $file_name);
+        exec($command, $out, $status);
+        if ($status == 0) {
+            return json_encode(['file' => $file_name]);
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Load match result
+     */
+    public function load(Request $request)
+    {
+        $file_name = base_path() . '/python/logs/' . $request->file;
+
+        $string = file_get_contents($file_name);
+        $data = json_decode($string,true);
+
+        $local = Team::find($data['local']['id']);
+        $visit = Team::find($data['visit']['id']);
+
+        $scorers = [[],[]];
+        foreach ($data['scorers'] as $scorer) {
+            if ($scorer[1] == 0) {
+                $scorers[0][] = $scorer[2] . ' <span>(' . substr($scorer[0], 0, 2) . '\')</span>';
+            } else {
+                $scorers[1][] = '<span>(' . substr($scorer[0], 0, 2) . '\')</span> ' . $scorer[2];
+            }
+        }
+
+        $local_rgb_primary = sscanf($local->primary_color, "#%02x%02x%02x");
+        $visit_rgb_primary = sscanf($visit->primary_color, "#%02x%02x%02x");
+
+        $actions = [];
+        foreach ($data['plays'] as $play) {
+            if (in_array($play[2], [6, 8, 12, 18, 19])) {
+                if (in_array($play[2], [6, 19])) {
+                    $play[3] = '<strong>' . $play[3] . '</strong>';
+                }
+                $actions[] = [substr($play[0], 0, 2), 'rgba(' . implode(', ', ($play[1] == 0) ? $local_rgb_primary : $visit_rgb_primary) . ', 1);', (($play[1] == 0) ? $local['text_color'] : $visit['text_color']), $play[3]];
+            }
+        }
+
+        $params = [
+            'local' => [
+                'name' => $local->name,
+                'primary_color' => $local->primary_color,
+                'secondary_color' => $local->secondary_color,
+                'rgb_primary' => $local_rgb_primary,
+                'text_color' => $local->text_color,
+                'goals' => $data['local']['goals'],
+                'formation' => $data['local']['formation'],
+                'posession' => $data['local']['posessionPer'],
+                'shots' => $data['local']['shots'],
+                'shots_goal' => $data['local']['shotsOnTarget'],
+            ],
+            'visit' => [
+                'name' => $visit->name,
+                'primary_color' => $visit->primary_color,
+                'secondary_color' => $visit->secondary_color,
+                'rgb_primary' => $visit_rgb_primary,
+                'text_color' => $visit->text_color,
+                'goals' => $data['visit']['goals'],
+                'formation' => $data['visit']['formation'],
+                'posession' => $data['visit']['posessionPer'],
+                'shots' => $data['visit']['shots'],
+                'shots_goal' => $data['visit']['shotsOnTarget'],
+            ],
+            'scorers' => $scorers,
+            'actions' => $actions,
+        ];
+
+        return view('match.result', $params);
+    }
+}
---
+++ resources/views/match/result.blade.php
@@ -0,0 +1,89 @@
+<div class="col-md-12 modal-match-result-teams">
+    <div class="col-md-4">
+        <h4>{{ $local['name'] }}</h4>
+        <div class="primarycolor" style="background-color:{{ $local['primary_color'] }};border-color:{{ ($local['primary_color'] == '#ffffff') ? $local['secondary_color'] : $local['primary_color'] }}"></div>
+        <div class="secondarycolor" style="background-color:{{ $local['secondary_color'] }};border-color:{{ ($local['secondary_color'] == '#ffffff') ? $local['primary_color'] : $local['secondary_color'] }}"></div>
+    </div>
+    <div class="col-md-4"><h4>{{ $local['goals'] }} - {{ $visit['goals'] }}</h4></div>
+    <div class="col-md-4">
+        <h4>{{ $visit['name'] }}</h4>
+        <div class="primarycolor" style="background-color:{{ $visit['primary_color'] }};border-color:{{ ($visit['primary_color'] == '#ffffff') ? $visit['secondary_color'] : $visit['primary_color'] }}"></div>
+        <div class="secondarycolor" style="background-color:{{ $visit['secondary_color'] }};border-color:{{ ($visit['secondary_color'] == '#ffffff') ? $visit['primary_color'] : $visit['secondary_color'] }}"></div>
+    </div>
+</div>
+<div class="col-md-12 modal-match-result-goals">
+    <div class="col-md-6">
+        <ul>
+            @foreach ($scorers[0] as $scorer)
+            <li>{!! $scorer !!}</li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-md-6">
+        <ul>
+            @foreach ($scorers[1] as $scorer)
+            <li>{!! $scorer !!}</li>
+            @endforeach
+        </ul>
+    </div>
+</div>
+<h3>Alineaciones</h3>
+<div class="col-md-12 modal-match-result-formations">
+    <div class="col-md-3">
+        <ul>
+            @for ($i = 0; $i < 18; $i++)
+                @if ($i <= 11)
+                <li>{{ $local['formation'][$i]['short_name'] }} <span>({{ $local['formation'][$i]['number'] }})</span></li>
+                @else
+                <li><span>{{ $local['formation'][$i]['short_name'] }} ({{ $local['formation'][$i]['number'] }})</span></li>
+                @endif
+            @endfor
+        </ul>
+    </div>
+    <div class="col-md-6">
+        <img src="{{ asset('img/field-large.png') }}">
+        @for ($i = 0; $i < 11; $i++)
+        <div class="player-container player-container-local" style="left:{{ $local['formation'][$i]['left'] }}%;top:{{ $local['formation'][$i]['top'] }}%;background-color:rgba({{ implode(', ', $local['rgb_primary']) }}, 0.5);border-color:{{ $local['secondary_color'] }};color:{{ $local['text_color'] }};">{{ $local['formation'][$i]['number'] }}</div>
+        <div class="player-container player-container-visit" style="left:{{ $visit['formation'][$i]['left'] }}%;top:{{ $visit['formation'][$i]['top'] }}%;background-color:rgba({{ implode(', ', $visit['rgb_primary']) }}, 0.5);border-color:{{ $visit['secondary_color'] }};color:{{ $visit['text_color'] }};">{{ $visit['formation'][$i]['number'] }}</div>
+        @endfor
+    </div>
+    <div class="col-md-3">
+        <ul>
+            @for ($i = 0; $i < 18; $i++)
+                @if ($i <= 11)
+                <li><span>({{ $visit['formation'][$i]['number'] }})</span> {{ $visit['formation'][$i]['short_name'] }}</li>
+                @else
+                <li><span>({{ $visit['formation'][$i]['number'] }}) {{ $visit['formation'][$i]['short_name'] }}</span></li>
+                @endif
+            @endfor
+        </ul>
+    </div>
+</div>
+<h3>Resumen</h3>
+<div class="col-md-12 modal-match-result-actions">
+    @foreach ($actions as $action)
+    <div class="col-md-12" style="background-color:{{ $action[1] }};color:{{ $action[2] }};">
+        <div class="col-md-1" style="text-align:center">{{ $action[0] }}'</div>
+        <div class="col-md-11">{!! $action[3] !!}</div>
+    </div>
+    @endforeach
+</div>
+<h3>Estadísticas</h3>
+<div class="col-md-12 modal-match-result-statistics">
+    <div class="col-md-12">
+        <div class="col-md-4 {{ $local['goals'] > $visit['goals'] ? 'best' : '' }}">{{ $local['goals'] }}</div>
+        <div class="col-md-4">Goles</div>
+        <div class="col-md-4 {{ $local['goals'] < $visit['goals'] ? 'best' : '' }}">{{ $visit['goals'] }}</div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-4 {{ $local['posession'] > $visit['posession'] ? 'best' : '' }}">{{ $local['posession'] }}</div>
+        <div class="col-md-4">Posesión</div>
+        <div class="col-md-4 {{ $local['posession'] < $visit['posession'] ? 'best' : '' }}">{{ $visit['posession'] }}</div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-4 {{ $local['shots'] > $visit['shots'] ? 'best' : '' }}">{{ $local['shots'] }} ({{ $local['shots_goal'] }})</div>
+        <div class="col-md-4">Disparos</div>
+        <div class="col-md-4 {{ $local['shots'] < $visit['shots'] ? 'best' : '' }}">{{ $visit['shots'] }} ({{ $visit['shots_goal'] }})</div>
+    </div>
+</div>
+<div class="clear"></div>
