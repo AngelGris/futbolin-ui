f15f63d55c08862c78454c24b78844cf909815c3
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-05-16 10:50:57 -0300

    Route grouping

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,30 @@
 Auth::routes();
 
 Route::get('/', 'Auth\LoginController@showLoginForm');
-Route::get('/contrasena/recuperar', ['as' => 'password.request', 'uses' => 'Auth\ResetPasswordController@showLinkRequestForm']);
-Route::get('/contrasena/recuperar/{token}', ['as' => 'password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+Route::group(['prefix' => 'contrasena'], function() {
+    Route::get('recuperar', ['as' => 'password.request', 'uses' => 'Auth\ResetPasswordController@showLinkRequestForm']);
+    Route::get('recuperar/{token}', ['as' => 'password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+});
 
 Route::get('/vestuario', ['as' => 'home', 'uses' => 'HomeController@index']);
 
-Route::get('/perfil/editar', ['as' => 'profile.edit', 'uses' => 'AccountSettingsController@index']);
-Route::patch('/perfil', ['as' => 'profile', 'uses' => 'AccountSettingsController@update']);
-Route::get('/perfil/contrasena', ['as' => 'profile.password', 'uses' => 'AccountSettingsController@editPassword']);
-Route::patch('/perfil/contrasena', 'AccountSettingsController@updatePassword');
+Route::group(['prefix' => 'perfil'], function() {
+    Route::patch('/', ['as' => 'profile', 'uses' => 'AccountSettingsController@update']);
+    Route::get('/editar', ['as' => 'profile.edit', 'uses' => 'AccountSettingsController@index']);
+    Route::get('/contrasena', ['as' => 'profile.password', 'uses' => 'AccountSettingsController@editPassword']);
+    Route::patch('/contrasena', 'AccountSettingsController@updatePassword');
+});
 
-Route::get('/equipo', 'TeamController@index');
-Route::post('/equipo', ['as' => 'team.store', 'uses' => 'TeamController@store']);
-Route::patch('/equipo', ['as' => 'team', 'uses' => 'TeamController@update']);
-Route::get('/equipo/crear', 'TeamController@create');
-Route::get('/equipo/editar', ['as' => 'team.edit', 'uses' => 'TeamController@edit']);
-Route::get('/equipo/estadisticas/{rival}', ['as' => 'team.statsVersus', 'uses' => 'TeamController@showStatsVersus']);
-Route::post('/equipo/estrategia', ['as' => 'team.strategy', 'uses' => 'TeamController@updateStrategy']);
-Route::post('/equipo/formacion', ['as' => 'team.formation', 'uses' => 'TeamController@updateFormation']);
+Route::group(['prefix' => 'equipo'], function() {
+    Route::get('/', 'TeamController@index');
+    Route::post('/', ['as' => 'team.store', 'uses' => 'TeamController@store']);
+    Route::patch('/', ['as' => 'team', 'uses' => 'TeamController@update']);
+    Route::get('/crear', 'TeamController@create');
+    Route::get('/editar', ['as' => 'team.edit', 'uses' => 'TeamController@edit']);
+    Route::get('/estadisticas/{rival}', ['as' => 'team.statsVersus', 'uses' => 'TeamController@showStatsVersus']);
+    Route::post('/estrategia', ['as' => 'team.strategy', 'uses' => 'TeamController@updateStrategy']);
+    Route::post('/formacion', ['as' => 'team.formation', 'uses' => 'TeamController@updateFormation']);
+});
 
 Route::get('/jugadores', ['as' => 'players', 'uses' => 'PlayerController@showListing']);
 
@@ -39,5 +45,7 @@ Route::get('/estrategia', ['as' => 'strategy', 'uses' => 'TeamController@showStr
 
 Route::get('/equipos', ['as' => 'teams', 'uses' => 'TeamController@showAll']);
 
-Route::post('/partido/jugar', ['as' => 'match.play', 'uses' => 'MatchController@play']);
-Route::get('/partido/cargar', ['as' => 'match.load', 'uses' => 'MatchController@load']);
+Route::group(['prefix' => 'partido'], function() {
+    Route::post('/jugar', ['as' => 'match.play', 'uses' => 'MatchController@play']);
+    Route::get('/cargar', ['as' => 'match.load', 'uses' => 'MatchController@load']);
+});
