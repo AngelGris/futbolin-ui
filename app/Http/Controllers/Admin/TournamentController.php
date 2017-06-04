7f0eb8f43c135687c8bd8528356d5b5082c61a45
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-06-04 16:06:54 -0300

    #47 Teams promotions

--- app/Http/Controllers/Admin/TournamentController.php
+++ app/Http/Controllers/Admin/TournamentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Tournament;
+use App\TournamentPosition;
 use App\Team;
 
 class TournamentController extends Controller
@@ -102,7 +103,80 @@ class TournamentController extends Controller
                 }
             }
         } else {
-            echo('not empty');
+            $teams = [];
+            $last_team = 0;
+            foreach ($last_tournament[0]->tournamentCategories as $category) {
+                $positions = TournamentPosition::where('category_id', '=', $category->id)->orderBy('position')->get();
+
+                $count = 0;
+                foreach ($positions as $position) {
+                    $team = Team::find($position->team_id);
+
+                    if ($team->user_id > 1) {
+                        $count++;
+                        if ($count <= 3) {
+                            if ($category->category > 1) {
+                                $teams[$category->category - 1][] = $team->id;
+                            } else {
+                                $teams[1][] = $team->id;
+                            }
+                        } else if ($count <= 17) {
+                            $teams[$category->category][] = $team->id;
+                        } else {
+                            if ($category->category < $request->categories) {
+                                $teams[$category->category + 1][] = $team->id;
+                            } else {
+                                $teams[$category->category][] = $team->id;
+                            }
+                        }
+
+                        if ($team->id > $last_team) {
+                            $last_team = $team->id;
+                        }
+                    }
+                }
+            }
+
+            for ($i = 1; $i < $request->categories; $i++) {
+                if (count($teams[$i]) < 20 * $zones) {
+                    $diff = (20 * $zones) - count($teams[$i]);
+                    for ($j = 0; $j < $diff; $j++) {
+                        $teams[$i][] = $teams[$i + 1][$j];
+                        unset($teams[$i + 1][$j]);
+                    }
+                }
+
+                $aux = [];
+                foreach ($teams[$i] as $team) {
+                    $aux[] = $team;
+                }
+                shuffle($aux);
+                $teams_added += count($teams[$i]);
+                for ($j = 0; $j < $zones; $j++) {
+                    $tournament->createCategory($i, $j + 1, array_slice($aux, $j * 20, 20));
+                }
+            }
+
+            if (($teams_count - $teams_added) > 0) {
+                $teams = Team::where('user_id', '>', 1)->where('id', '>', $last_team)->get();
+                $groups_remaining = (int)(count($teams) / 20);
+                if (count($teams) % 20) {
+                    $groups_remaining++;
+                }
+
+                $sparrings = Team::where('user_id', '=', 1)->limit((20 * $groups_remaining) - count($teams))->inRandomOrder()->get();
+                $aux = [];
+                foreach ($teams as $team){
+                    $aux[] = $team->id;
+                }
+                foreach ($sparrings as $team) {
+                    $aux[] = $team->id;
+                }
+                shuffle($aux);
+                for ($j = 0; $j < $zones; $j++) {
+                    $tournament->createCategory($i, $j + 1, array_slice($aux, $j * 20, 20));
+                }
+            }
         }
 
         return redirect(route('admin.tournaments', getDomain()));
