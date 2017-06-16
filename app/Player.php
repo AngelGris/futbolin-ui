5e2a02d501d75a3558c9a426bbab9c10a1c3389a
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-06-16 18:57:36 -0300

    Limit players attributs to 99 max

--- app/Player.php
+++ app/Player.php
@@ -149,34 +149,34 @@ class Player extends Model
             for ($i = 0; $i < $points; $i++) {
                 switch (mt_rand(0, 9)) {
                     case 0:
-                        $this->goalkeeping++;
+                        $this->goalkeeping = min($this->goalkeeping + 1, 99);
                         break;
                     case 1:
-                        $this->defending++;
+                        $this->defending = min($this->defending + 1, 99);
                         break;
                     case 2:
-                        $this->dribbling++;
+                        $this->dribbling = min($this->dribbling + 1, 99);
                         break;
                     case 3:
-                        $this->heading++;
+                        $this->heading = min($this->heading + 1, 99);
                         break;
                     case 4:
-                        $this->jumping++;
+                        $this->jumping = min($this->jumping + 1, 99);
                         break;
                     case 5:
-                        $this->passing++;
+                        $this->passing = min($this->passing + 1, 99);
                         break;
                     case 6:
-                        $this->precision++;
+                        $this->precision = min($this->precision + 1, 99);
                         break;
                     case 7:
-                        $this->speed++;
+                        $this->speed = min($this->speed + 1, 99);
                         break;
                     case 8:
-                        $this->strength++;
+                        $this->strength = min($this->strength + 1, 99);
                         break;
                     case 9:
-                        $this->tackling++;
+                        $this->tackling = min($this->tackling + 1, 99);
                         break;
 
                 }
