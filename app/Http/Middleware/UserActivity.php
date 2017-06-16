ca0c4c0758141d273d2ff598baf70efc249502ec
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2017-06-16 10:45:13 -0300

    #50 last activity

---
+++ app/Http/Middleware/UserActivity.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class UserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $user->last_activity = date('Y-m-d H:i:s');
+            $user->save();
+        }
+
+        return $next($request);
+    }
+}
