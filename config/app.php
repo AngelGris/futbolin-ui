3a5e9b94332eee229f81a5bd39fe6066b7701e9e
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-02-03 17:27:22 -0300

    Install debugbar

--- config/app.php
+++ config/app.php
@@ -178,6 +178,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Debugbar
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
