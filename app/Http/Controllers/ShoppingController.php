19f8a5639c048205f857ab779940a10ea18788d5
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-05-19 11:14:22 -0300

    Train team when trainner is hired

--- app/Http/Controllers/ShoppingController.php
+++ app/Http/Controllers/ShoppingController.php
@@ -70,6 +70,7 @@ class ShoppingController extends Controller
             case 3:
                 $user->team->trainer = Carbon::now()->addWeeks(1);
                 $user->team->save();
+                $user->team->train();
                 $success_message = 'El entrenador ha sido contratado hasta el ' . $user->team->trainer->format('d/m/Y H:i:s') . '.';
                 break;
             case 4:
