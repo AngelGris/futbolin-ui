473fbe63cd46117b132483f9e0c6efe36ec4ca6a
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-05-23 11:44:24 -0300

    topic-3-financialSystem

---
+++ resources/views/team/finances.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.inner')
+
+@section('content-inner')
+@if (empty($movements))
+<h3>No hay movimientos</h3>
+@else
+{{ $movements->links() }}
+<table id="dyntable" class="table table-bordered table-players responsive">
+    <thead>
+        <tr>
+            <th>Fecha</th>
+            <th>Descripción</th>
+            <th>Monto</th>
+            <th>Balance</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($movements as $movement)
+        <tr>
+            <td style="text-align: right;">{{ $movement->created_at->format('d/m/Y') }}</td>
+            <td>{{ $movement->description }}</td>
+            <td style="text-align: right;">{!! formatCurrency($movement->amount) !!}</td>
+            <td style="text-align: right;">{!! formatCurrency($movement->balance) !!}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+{{ $movements->links() }}
+@endif
+@endsection
