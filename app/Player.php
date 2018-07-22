63ee0f37ecfb8b9582d839129c05db1afe207092
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-07-22 11:28:06 -0300

    Set best_offer_value when player is free

--- app/Player.php
+++ app/Player.php
@@ -472,9 +472,10 @@ class Player extends Model
         $this->save();
 
         PlayerSelling::create([
-            'player_id' => $this->id,
-            'value'     => $this->value,
-            'closes_at' => Carbon::now()->addDays(\Config::get('constants.PLAYERS_TRANSFERABLE_PERIOD'))
+            'player_id'         => $this->id,
+            'value'             => $this->value,
+            'best_offer_value'  => $this->value,
+            'closes_at'         => Carbon::now()->addDays(\Config::get('constants.PLAYERS_TRANSFERABLE_PERIOD'))
         ]);
 
         return $this;
