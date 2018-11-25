927a9373b6d3b75cbd14b8d9136e8955c48661e7
AngelGris <lucianogarciabes@gmail.com>
2018-11-25 15:35:54 +0100

    Contact form

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="loginpanel" style="width:500px;">
     <div class="loginpanelinner">
-        <h1>Contacto</h1>
+        <h1>@lang('labels.contact')</h1>
         <form id="contact" action="{{ route('contact') }}" method="POST" role="form">
             {{ csrf_field() }}
             <div class="inputwrapper">
-                <input id="name" type="text" name="name" value="{{ old('name') }}" placeholder="Nombre completo" class="form-control" required autofocus>
+                <input id="name" type="text" name="name" value="{{ old('name') }}" placeholder="@lang('labels.full_name')" class="form-control" required autofocus>
                 @if ($errors->has('name'))
                 <label class="error">
                     <strong>{{ $errors->first('name') }}</strong>
@@ -15,7 +15,7 @@
                 @endif
             </div>
             <div class="inputwrapper">
-                <input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="Dirección de e-mail" class="form-control" required>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="@lang('labels.email_address')" class="form-control" required>
                 @if ($errors->has('email'))
                 <label class="error">
                     <strong>{{ $errors->first('email') }}</strong>
@@ -28,10 +28,10 @@
                     <strong>{{ $errors->first('message') }}</strong>
                 </label>
                 @endif
-                <textarea id="message" name="message" placeholder="Mensaje" class="form-control" style="height:100px;" required>{{ old('message') }}</textarea>
+                <textarea id="message" name="message" placeholder="@lang('labels.message')" class="form-control" style="height:100px;" required>{{ old('message') }}</textarea>
             </div>
             <div class="inputwrapper">
-                <button type="submit" class="btn btn-primary">Enviar</button>
+                <button type="submit" class="btn btn-primary">@lang('labels.send')</button>
             </div>
             <div class="clear"></div>
         </form>
