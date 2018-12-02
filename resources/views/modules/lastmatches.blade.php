8d926f9e11c1e41ad0c262a17d3279f8745b1797
AngelGris <lucianogarciabes@gmail.com>
2018-12-02 18:34:57 +0100

    Matches summary modal

--- resources/views/modules/lastmatches.blade.php
+++ resources/views/modules/lastmatches.blade.php
@@ -1,6 +1,6 @@
 @if (!empty($last_matches))
 <div class="col-md-6 zebra" id="home-last-matches">
-    <h3 style="margin-bottom:20px;text-align:center;">Últimos partidos</h3>
+    <h3 style="margin-bottom:20px;text-align:center;">@lang('labels.last_matches')</h3>
     @foreach ($last_matches as $match)
     <div class="col-xs-12"{!! $match['won'] ? ' style="font-weight:bold;"' : '' !!}>
         <div class="col-xs-2">{{ $match['date'] }}</div>
@@ -15,7 +15,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Cargando resumen del partido</h4>
+                <h4 class="modal-title">@lang('labels.loading_match_summary')</h4>
             </div>
             <div class="modal-body modal-match-result" id="modal-match-loading-content">
                 <img src="{{ asset('img/loader.gif') }}" />
@@ -30,13 +30,13 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Resumen del partido</h4>
+                <h4 class="modal-title">@lang('labels.match_summary')</h4>
             </div>
             <div class="modal-body modal-match-result" id="modal-match-result-content">
                 <img src="{{ asset('img/loader.gif') }}" />
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">@lang('labels.close')</button>
             </div>
         </div>
     </div>
