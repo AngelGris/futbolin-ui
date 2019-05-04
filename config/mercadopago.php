a48d6eb11f48f4e162f71d2988b788667bb00fd4
AngelGris <lucianogarciabes@gmail.com>
2019-05-04 16:30:45 +0200

    Remove unused file

--- config/mercadopago.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    /**
-     * Set our Sandbox and Live credentials
-     */
-    'mode' => env('MERCADOPAGO_MODE', ''),
-    'sandbox_access_token' => env('MERCADOPAGO_SANDBOX_ACCESS_TOKEN', ''),
-    'live_access_token' => env('MERCADOPAGO_LIVE_ACCESS_TOKEN', ''),
-];
